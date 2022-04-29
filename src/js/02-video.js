fb2de8b4ffd7e1a4096ff29f83af9d66bb80531b
Larin Artem <larin_art@ukr.net>
2022-04-29 12:44:45 +0300

    task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('Play!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const playedSeconds = localStorage.getItem("videoplayer-current-time");
+
+
+player.setCurrentTime(Number(playedSeconds)).then(function(seconds) {
+        seconds = Number(playedSeconds);
+        console.log(seconds);
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                console.log('The time was less than 0 or greater than the video’s duration');
+                break;
+    
+            default:
+                console.log('Some other error occurred');
+                break;
+        }
+    });
