ff6a7fdfe67ba5ff2c3763a6670e30e9671d9850
Larin Artem <larin_art@ukr.net>
2022-04-29 12:10:36 +0300

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+let instance = null;
+
+const createGallery = (galleryItems) =>
+    galleryItems.map(({ preview, original, description }) =>
+        `<div class="gallery__item">
+            <a class="gallery__link"
+                href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`).join('');   
+
+gallery.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+const galleryOptions = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '300ms',
+});
+
+galleryOptions.addEventListener('click', onImgClick);
+
+function onEscPress(e){
+    if (e.code === 'Escape') {
+        instance.close();
+    };
+};
+
+galleryOptions.addEventListener('click', onEscPress);
