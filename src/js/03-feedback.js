4f4f574405fbf525d0423e66e1b0ddc3caaea221
Larin Artem <larin_art@ukr.net>
2022-04-30 14:08:48 +0300

    task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+console.log(form);
+const LOCALSTORAGE_KEY = "feedback-form-state";
+let data = {};
+
+form.addEventListener('input', throttle(onInputDataSave, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onInputDataSave(e) {
+    data[e.target.name] = e.target.value;
+    const storageDate = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({...storageDate,...data}));
+};
+
+function onFormSubmit (e) {
+    e.preventDefault(); 
+    e.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    data = {};
+};
+function savedDataOutput () {
+    const savedDataForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+if(savedDataForm?.email){
+    form.email.value = savedDataForm.email; 
+}
+if(savedDataForm?.message){
+    form.message.value = savedDataForm.message;
+}
+};
+savedDataOutput();
